92974d96adf3b2cc8b5e7e903167c9319e25ccc4
AK91007 <adithyakulgod@outlook.com>
2022-02-05 16:29:52 +0530

    Adding close_po app after updating in working folder

---
+++ close_po/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Close Purchase',
+    'version': '14.0.1',
+    'category': '',
+    'summary': 'For all',
+    'author': "Adithya",
+    'description': """
+This module is used to identify the PO which are closed
+    """,
+    'depends': ['base','purchase'],
+    'data': [
+        'views/close_po.xml'
+       
+    ],
+   
+    'installable': True,
+    'auto_install': False
+}
